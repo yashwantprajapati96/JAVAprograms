9b10836336d2cf1cd36a52218f990814ad5f70e6
Yashwant Prajapati <yashprajapati5383@gmail.com>
2025-12-16 10:52:55 +0530

    Add Multiplication class with main method
    Implement a simple multiplication program that multiplies two integers and prints the result.

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+
+
+package com.mycompany.multiplication;
+
+
+public class Multiplication {
+    
+
+    public static void main(String[] args) {
+        int a=12;
+    int b=6;
+    int c=a*b;
+        System.out.println(a*b);
+        System.out.println(c);
+    }
+}
