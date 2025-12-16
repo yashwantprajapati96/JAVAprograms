217e73569baad9db0d7906ad2d8e552b33634207
Yashwant Prajapati <yashprajapati5383@gmail.com>
2025-12-16 10:57:37 +0530

    Create Average.java

---
+++ Average.java
@@ -0,0 +1,17 @@
+
+
+package com.mycompany.Average;
+
+
+public class Average {
+    
+
+    public static void main(String[] args) {
+        int a=12;
+    int b=6;
+    int c=2;
+    int d=4;
+    int average=(a+b+c+d)/4;
+        System.out.println(average);
+    }
+}
