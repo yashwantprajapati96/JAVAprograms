8f8753274849f929e4092efa90d3fc6162d32d98
Yashwant Prajapati <yashprajapati5383@gmail.com>
2025-12-21 01:30:17 +0530

    Create ConditionalStatement.java

---
+++ ConditionalStatement.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.loops;
+
+
+public class Loops {
+
+    public static void main(String[] args) {
+         int age=20;
+         if(age>=18){
+             System.out.println("eliggible for vote");
+         }
+         else{
+             System.out.println("not eliggible for vote");
+         }
+    }
+}
